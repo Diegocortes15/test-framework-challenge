275805451814bfe8309482c534f067d55b8e723a
Diego <cortesroadiegoalejandro@gmail.com>
2022-09-06 17:27:55 -0500

    main: Added Athentication test cases

---
+++ src/test/java/Authentication.java
@@ -0,0 +1,58 @@
+import org.themoviedb.auth.AuthConstants;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class Authentication {
+
+    @Test
+    public void createRequestToken() {
+
+        HashMap<String, String> headers = new HashMap<>();
+        headers.put("Authorization", "Bearer " + AuthConstants.USER_ACCESS_TOKEN);
+
+        given()
+                .baseUri("https://api.themoviedb.org/4/auth")
+                .headers(headers)
+                .queryParam("api_key", AuthConstants.USER_API_KEY)
+        .when()
+                .post("/request_token")
+        .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .body("$", hasKey("request_token"),
+                        "status_message", is(equalTo("Success.")));
+    }
+
+    @Test
+    public void createAccessToken(){
+
+        HashMap<String, String> headers = new HashMap<>();
+        headers.put("Authorization", "Bearer " + AuthConstants.USER_ACCESS_TOKEN);
+        headers.put("Content-type", "application/json");
+
+        String json = String.format("""
+                {
+                    "request_token": "%s"
+                }
+                """, AuthConstants.USER_REQUEST_TOKEN);
+
+        given()
+                .baseUri("https://api.themoviedb.org/4/auth")
+                .headers(headers)
+                .queryParam("api_key", AuthConstants.USER_API_KEY)
+                .body(json)
+        .when()
+                .post("/access_token")
+        .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .body("$",hasKey("access_token"),
+                        "status_message", is(equalTo("Success.")));
+    }
+}
