a3c9d89abec2c56f1d0bba25da20ffd8bb586f53
Diego <cortesroadiegoalejandro@gmail.com>
2022-09-07 14:14:02 -0500

    main: Added Log4j2 to log test cases steps

--- src/test/java/AuthSpec.java
+++ src/test/java/AuthSpec.java
@@ -1,4 +1,6 @@
 import io.qameta.allure.Description;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.themoviedb.auth.AuthConstants;
 import org.testng.annotations.Test;
 
@@ -8,11 +10,14 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class AuthSpec {
+    private  static final Logger logger = LogManager.getLogger("authSpecFileLogger");
 
     @Test
     @Description("Create request token given api_key and access_token")
     public void createRequestToken() {
 
+        logger.info("Create a list of movies given a name");
+        logger.info("Setting headers");
         HashMap<String, String> headers = new HashMap<>();
         headers.put("Authorization", "Bearer " + AuthConstants.USER_ACCESS_TOKEN);
 
@@ -28,16 +33,20 @@ public class AuthSpec {
                 .statusCode(200)
                 .body("$", hasKey("request_token"),
                         "status_message", is(equalTo("Success.")));
+        logger.info("Verifying that request_token exist and status_message is Success.");
     }
 
     @Test
     @Description("Create access token given api_key and access_token from request token")
     public void createAccessToken(){
 
+        logger.info("Create access token given api_key and access_token from request token");
+        logger.info("Setting headers");
         HashMap<String, String> headers = new HashMap<>();
         headers.put("Authorization", "Bearer " + AuthConstants.USER_ACCESS_TOKEN);
         headers.put("Content-type", "application/json");
 
+        logger.info("Setting requestBody to create a movie list");
         String json = String.format("""
                 {
                     "request_token": "%s"
@@ -57,5 +66,6 @@ public class AuthSpec {
                 .statusCode(200)
                 .body("$",hasKey("access_token"),
                         "status_message", is(equalTo("Success.")));
+        logger.info("Verifying that access_token exist and status_message is Success.");
     }
 }
--- src/test/java/ListSpec.java
+++ src/test/java/ListSpec.java
@@ -1,5 +1,7 @@
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 import org.themoviedb.auth.AuthConstants;
 import org.themoviedb.list.ListConstants;
@@ -7,14 +9,20 @@ import org.themoviedb.list.ListConstants;
 import java.util.HashMap;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 @Feature("List Tests")
 public class ListSpec {
 
+    private  static final Logger logger = LogManager.getLogger("listSpecFileLogger");
+
     @Test
     @Description("Create a list of movies given a name")
     public void createList() {
+
+        logger.info("Create a list of movies given a name");
+        logger.info("Setting requestBody to create a movie list");
         String json = String.format("""
                 {
                     "name": "%s",
@@ -22,10 +30,12 @@ public class ListSpec {
                 }
                 """, ListConstants.LIST_NAME);
 
+        logger.info("Setting headers");
         HashMap<String, String> headers = new HashMap<>();
         headers.put("Authorization", "Bearer " + AuthConstants.USER_APP_ACCESS_TOKEN);
         headers.put("Content-type", "application/json");
 
+        logger.info("Making a POST request with /list resource to create a new list");
         given()
                 .baseUri("https://api.themoviedb.org/4")
                 .headers(headers)
@@ -34,19 +44,25 @@ public class ListSpec {
         .when()
                 .post("/list")
         .then()
-                .log().all()
+                .log().body()
                 .assertThat()
                 .statusCode(201)
                 .body("status_message", is(equalTo("The item/record was created successfully.")),
                         "success", is(true));
+
+        logger.info("Making assertions from response body");
     }
 
     @Test
     @Description("Get a list of movies given an id")
     public void getList() {
+
+        logger.info("Create a list of movies given a name");
+        logger.info("Setting headers");
         HashMap<String, String> headers = new HashMap<>();
         headers.put("Authorization", "Bearer " + AuthConstants.USER_APP_ACCESS_TOKEN);
 
+        logger.info("Making a POST request with /list resource to create a new list");
         given()
                 .baseUri("https://api.themoviedb.org/4")
                 .headers(headers)
@@ -59,12 +75,16 @@ public class ListSpec {
                 .statusCode(200)
                 .body("id", is(equalTo(ListConstants.LIST_ID)),
                         "name", is(equalTo(ListConstants.LIST_NAME)));
+
+        logger.info("Making assertions from response body");
     }
 
     @Test
     @Description("Add movies in a list given an id")
     public void addItems() {
 
+        logger.info("Add movies in a list given an id");
+        logger.info("Setting requestBody to create a movie list");
         String json = """
                 {
                     "items": [
@@ -88,6 +108,7 @@ public class ListSpec {
         headers.put("Authorization", "Bearer " + AuthConstants.USER_APP_ACCESS_TOKEN);
         headers.put("Content-type", "application/json");
 
+        logger.info("Making a POST request with /list resource to create a new list");
         given()
                 .baseUri("https://api.themoviedb.org/4")
                 .headers(headers)
@@ -108,5 +129,7 @@ public class ListSpec {
                         "results[2].media_type", is(equalTo("tv")),
                         "results[2].media_id", is(equalTo(1396))
                 );
+
+        logger.info("Making assertions from response body");
     }
 }
